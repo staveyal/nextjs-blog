5c82e7cb9b9bb370371cedc42d44f15e845f6176
Stav Eyal <staveyal210@gmail.com>
2020-09-22 14:01:01 +0300

    Index page refrences new TS file name

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.tsx</code>
         </p>
 
         <div className="grid">
